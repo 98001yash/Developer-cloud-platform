79e1044cb87eef4ebf4672b4e7ac3507f97eefef
98001yash <yashchauhan.gaya@gmail.com>
2026-02-07 11:48:03 +0530

    configuration done of the api-gateway

--- api-gateway/src/main/java/com/devcloud/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/devcloud/api_gateway/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.redditApp.api_gateway;
+package com.devcloud.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
