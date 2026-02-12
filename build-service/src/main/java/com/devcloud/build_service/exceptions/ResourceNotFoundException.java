3af85462440b8cdc08c8cd2aa61f1e9582479731
98001yash <yashchauhan.gaya@gmail.com>
2026-02-12 17:10:57 +0530

    added the advices, auth and exceptions package

---
+++ build-service/src/main/java/com/devcloud/build_service/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.devcloud.build_service.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException() {
+    }
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ build-service/src/main/java/com/devcloud/build_service/exceptions/RuntimeConflictException.java
@@ -0,0 +1,11 @@
+package com.devcloud.build_service.exceptions;
+
+public class RuntimeConflictException extends RuntimeException{
+    public RuntimeConflictException() {
+    }
+
+    public RuntimeConflictException(String message) {
+        super(message);
+    }
+}
+
