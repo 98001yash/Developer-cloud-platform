c1d0ee00af5e740891634d6a7a585d52d77135f5
98001yash <yashchauhan.gaya@gmail.com>
2026-02-12 17:06:50 +0530

    added the new microservice called build service

---
+++ build-service/src/test/java/com/devcloud/build_service/BuildServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devcloud.build_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
