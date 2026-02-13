d69a342aaeb0eb075741a7bfbd77ce109361b7d8
98001yash <yashchauhan.gaya@gmail.com>
2026-02-13 09:37:41 +0530

    added the deployment microservice

---
+++ deployment-service/src/test/java/com/devcloud/deployment_service/DeploymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devcloud.deployment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeploymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
