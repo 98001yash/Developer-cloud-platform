5fc08de53dfe3556cd7edaab19e690618a4f3f2f
98001yash <yashchauhan.gaya@gmail.com>
2026-02-13 11:17:50 +0530

    added tge deployment entity

---
+++ deployment-service/src/main/java/com/devcloud/deployment_service/entity/Deployment.java
@@ -0,0 +1,60 @@
+package com.devcloud.deployment_service.entity;
+
+
+import com.devcloud.deployment_service.enums.DeploymentStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "deployments")
+public class Deployment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "project_id", nullable = false)
+    private Long projectId;
+
+    @Column(name = "build_id", nullable = false)
+    private Long buildId;
+
+
+    @Column(name = "image_name", nullable = false)
+    private String imageName;
+
+    @Column(name = "container_id")
+    private String containerId;
+
+    // Host port assigned
+    @Column(name = "port")
+    private Integer port;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private DeploymentStatus status;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+    @PrePersist
+    public void onCreate() {
+        this.createdAt = Instant.now();
+        this.updatedAt = Instant.now();
+    }
+
+    @PreUpdate
+    public void onUpdate() {
+        this.updatedAt = Instant.now();
+    }
+}
